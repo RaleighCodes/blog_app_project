c24dacf33275d2cffdbf9546b53a5bcab266161d
James <james.raleigh@gmail.com>
2022-08-21 19:41:06 -0400

    added pages and heaers

--- components/Header.jsx
+++ components/Header.jsx
@@ -6,7 +6,7 @@ const categories = [{name: 'React', slug:'react'}, {name: 'Web Development', slu
 const Header = () => {
   return (
     <div className="container mx-auto px-10 mb-8">
-        <div className='boarder-b w-full inline-block border-blue-400 py-8'>
+        <div className='boarder-b w-full inline-block border-black-400 py-8'>
             <div className='md:float-left block'>
                 <Link href={"/"}>
                     <span className='cursor-pointer font-bold text-4xl text-black'>
@@ -15,8 +15,8 @@ const Header = () => {
                 </Link>
             </div>
             <div className='hidden md:float-left md:contents'>
-                {categories.map((category) => (
-                    <Link key={category.slug} href={`/category/${category.slug}`}>
+                {categories.map((category, index) => (
+                    <Link key={index} href={`/category/${category.slug}`}>
                         <span className='md:float-right mt-2 align-middle text-black ml-4 font-semibold cursor-pointer'>
                             {category.name}
                         </span>
